d3e163fdbd909221be4c333f770ff60cdcd4677c
Claire <clairetanhj@gmail.com>
2020-12-08 16:19:28 +0800

    add scale and display position

--- UnityHinge/Assets/Scripts/Rotator.cs
+++ UnityHinge/Assets/Scripts/Rotator.cs
@@ -5,17 +5,21 @@ using UnityEditor;
 
 public class Rotator : MonoBehaviour
 {
-    public float maxbound;
-    public float minbound;
-    public float angleFromOrigin;
-    
+    public float maxAngle;
+    public float minAngle;
+    public float upperBound;
+    public float lowerBound;
+
+    [SerializeField] private float angleFromOrigin;
     private float angle;
-    private float newangle;
     private float origin;
     private float neworigin;
+    private float position;
+    private float newposition;
 
     void Start()
     {        
+        // load origin from save.json
         if (System.IO.File.Exists(Application.dataPath + "/save.json")) {
             string saveString = System.IO.File.ReadAllText(Application.dataPath + "/save.json");
             SaveAngle loadedData = JsonUtility.FromJson<SaveAngle>(saveString);
@@ -25,13 +29,16 @@ public class Rotator : MonoBehaviour
             origin = UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x;
         }
         
-        neworigin = angle = newangle = origin;
-
+        neworigin = angle = origin;
         Debug.Log("Origin: " + origin);
+
+        newposition = position = (angleFromOrigin-minAngle) / (maxAngle-minAngle) * (upperBound-lowerBound) + lowerBound;
+        Debug.Log("Position in Scale: " + position);
     }
 
     void Update()
     {   
+        // set value of new origin and save it to save.json
         if(origin != neworigin) {
             origin = neworigin;
             Debug.Log("Origin: " + origin);
@@ -41,15 +48,24 @@ public class Rotator : MonoBehaviour
             System.IO.File.WriteAllText(Application.dataPath + "/save.json", json);
         }  
 
+        // calculate angleFromOrigin from global angle
         angleFromOrigin = angle - origin;
         if(angleFromOrigin>180) angleFromOrigin -= 360;
         if(angleFromOrigin<-180) angleFromOrigin += 360;
 
-        if(Input.GetAxisRaw("Vertical") == 1 && angleFromOrigin < maxbound) {
+        // calculate and print position in scale
+        newposition = (angleFromOrigin-minAngle) / (maxAngle-minAngle) * (upperBound-lowerBound) + lowerBound;
+        if(position != newposition) {
+            position = newposition;
+            Debug.Log("Position in Scale: " + position);
+        }
+
+        // rotate arm according to input
+        if(Input.GetAxisRaw("Vertical") == 1 && angleFromOrigin < maxAngle) {
             transform.Rotate(1.0f, 0.0f, 0.0f);
         }
 
-        if(Input.GetAxisRaw("Vertical") == -1 && angleFromOrigin > minbound) {
+        if(Input.GetAxisRaw("Vertical") == -1 && angleFromOrigin > minAngle) {
             transform.Rotate(-1.0f, 0.0f, 0.0f);
         }
    
@@ -57,12 +73,8 @@ public class Rotator : MonoBehaviour
             transform.localRotation = Quaternion.Euler(origin, 0.0f, 0.0f);
         }
 
-        newangle = UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x;
-        
-        if(angle != newangle) {
-            angle = newangle;
-            Debug.Log("Global Angle: " + angle);
-        }
+        //set value of new global angle
+        angle = UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x;
     }
 
     public void SetOrigin() {
